ca62c7b57217106b473621ea48d95a794383a2c6
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-03-12 20:48:54 -0300

    removed unecessary code

--- src/client/lib/main.dart
+++ src/client/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:client/features/screens/home_page_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,35 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-          ],
-        ),
-      ),
+      home: const HomePageScreen(title: 'Flutter Demo Home Page'),
     );
   }
 }
